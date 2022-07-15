3b94445b2691fe9825728b338662f0dc8be705d7
Jan Provazník <proovaznik@gmail.com>
2022-07-15 20:22:39 +0200

    FEAT: icons provider

---
+++ SpaceX/SpaceX/Utils/Icons.swift
@@ -0,0 +1,22 @@
+//
+//  Icons.swift
+//  SpaceX
+//
+//  Created by Jan Provaznik on 15.07.2022.
+//
+
+import SwiftUI
+
+enum Icons: String {
+    case burn = "Burn"
+    case engine = "Engine"
+    case fuel = "fuel"
+    case reusable = "Reusable"
+    case flyingRocket = "Rocket Flying"
+    case rocketIdle = "Rocket Idle"
+    case rocket = "Rocket"
+
+    var image: Image {
+        Image(self.rawValue)
+    }
+}
