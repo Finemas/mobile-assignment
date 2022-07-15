eaeb7027859249be207da6bc3c0bd27a72d4705d
Jan Provazník <proovaznik@gmail.com>
2022-07-15 18:12:43 +0200

    FEAT: project init

---
+++ SpaceX/SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Jan Provaznik on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
